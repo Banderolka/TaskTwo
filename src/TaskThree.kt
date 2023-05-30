b4ea5c4adf6874bbf53534bc79ba95e42859ebcf
Banderolka <bothisava@gmail.com>
2023-05-30 22:01:17 +0300

    Задание № 3

--- src/TaskThree.kt
+++ src/TaskThree.kt
@@ -1,5 +1,9 @@
-fun main (args: Array<String>) {
+import kotlin.math.sqrt
 
+fun main (args: Array<String>) {
+    /*
+    Задание № 12
+     */
 
     fun determineLeapYear (year: Int){
         if (year % 4 == 0 ) println("$year - високосный год")
@@ -11,4 +15,52 @@ fun main (args: Array<String>) {
     var number: Int = userEnter.toString().toInt()
 
     determineLeapYear(number)
+
+    /*
+    Задание № 13
+     */
+
+    fun returnQuantityEmployees (vararg employees: String){
+        var result = 0
+        for (n in employees){
+            result++
+        }
+
+        println("Сотрудников всего $result")
+    }
+
+    /*
+    Задание № 14
+     */
+
+    fun employeeInformation (fullName: String, jobTitle: String){
+        println("ФИО - $fullName\nДолжность - $jobTitle")
+    }
+
+    fun employeeInformation (fullName: String, age: Int, jobTitle: String){
+        println("ФИО - $fullName\nВозвраст - $age\nДолжность - $jobTitle")
+    }
+
+    fun employeeInformation(fullName: String, familyStatus: String, jobTitle: String){
+        println("ФИО - $fullName\nСемейное положение - $familyStatus\nДолжность - $jobTitle")
+    }
+
+    fun employeeInformation (fullName: String, age: Int, familyStatus: String, jobTitle: String){
+        println("ФИО - $fullName\nВозвраст - $age\nСемейное положение - $familyStatus\nДолжность - $jobTitle")
+    }
+
+    /*
+    Задание № 15
+     */
+
+    val str = {x: Array<String> -> println(x.contentToString())}
+
+    /*
+    Задание № 16
+     */
+
+    fun Double.squareRoot(): Double {
+        return sqrt(this)
+    }
+
 }
